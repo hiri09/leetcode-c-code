c4c004383a1357ff0d95883b8f6b0aa589979c4c
himanshu kumar <hk7703940916@gmail.com>
2023-02-08 16:45:25 +0530

    changes rightnow

--- p1.cpp
+++ p1.cpp
@@ -1,51 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
-bool isvalid(string s){
-    if(s[0]=='0'){
-        return false;
-    }
-    int n=stoi(s);
-    if(n>255){
-        return false;
-    }
-    cout<<s<<endl;
-    return true;
+bool change(pair<char,int>a,pair<char,int>b){
+
 }
-void fun(string s,vector<string> &ans,string work,int idx,int p){
-    if(idx==s.length()){
-        if(p==4){
-            ans.push_back(work.substr(0,work.length()-1));
-        }
-        return;
-    }
-    fun(s,ans,work+s[idx]+'.',idx+1,p+1);
-    if(idx+2<=s.length() && isvalid(s.substr(idx,2))==true){
-        fun(s,ans,work+s.substr(idx,2)+'.',idx+2,p+1);
-    }
-    if(idx+3<=s.length() && isvalid(s.substr(idx,3))==true){
-        fun(s,ans,work+s.substr(idx,3)+'.',idx+3,p+1);
-    }
-}
-vector<string> solve(string s){
-   vector<string>ans;
-   if(s.length()>12){
-     return ans;
-   }
+void solve(string s){
+   unordered_map<char,int>mp;
    for(auto it:s){
-    if(it>='0' && it<='9'){
-        continue;
-    }
-     return ans;
+      mp[it]++;
    }
-   fun(s,ans,"",0,0);
-   return ans;
+
+  
 }
 int main(){
-    string s;
-    cin>>s;
-    vector<string>ans=solve(s);
-    for(auto it:ans){
-        cout<<it<<" "<<endl;
-    }
-    return 0;
+   return 0;
 }
